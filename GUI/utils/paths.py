a7212ecbf9a1071da814bcfb012e99bae1194c92
Amit Kumar <amitasviper@gmail.com>
2018-04-17 18:59:05 -0700

    Create separate file for sub operations

--- GUI/utils/paths.py
+++ GUI/utils/paths.py
@@ -7,7 +7,9 @@ from commands import Commands
 
 from qc_page import generateQCPage
 
-class Operations(object):
+from sub_operation import SubOperation
+
+class Operations(object, SubOperation):
 	def __init__(self, controller):
 		self.controller = controller
 		self.logger = controller.logger
@@ -384,22 +386,6 @@ class Operations(object):
 			self.com.runFslEyes(anatomical_file_path, bet_brain_file, image_path)
 		generateQCPage('bet', image_files_base)
 		self.logger.info('Brain extraction completed for all subjects')
-
-
-	def runWMSegmentation(self):
-		# Skip this step if user has already performed brain extraction
-		if self.controller.b_wm_segmentation.get() == True or self.skip: return False
-		for subject in self.subjects:
-			anatomical_file_path, lesion_files = self._setSubjectSpecificPaths_1(subject)
-			((t1_mgz, seg_file), bet_brain_file, wm_mask_file) = self._setSubjectSpecificPaths_2(subject)
-			
-			self.com.runFast(os.path.join(self.getIntermediatePath(subject), subject), bet_brain_file)
-
-			image_files_base = os.path.join(self.output_directory, 'QC_WM')
-			image_path = os.path.join(image_files_base, subject + '_WM.png')
-			self.com.runFslEyes(anatomical_file_path, wm_mask_file, image_path)
-		generateQCPage('WM', image_files_base)
-		self.logger.info('White Matter segmentation completed for all subjects')
 
 
 	def getTemplateBrainROIS(self):

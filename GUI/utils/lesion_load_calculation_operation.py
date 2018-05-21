cde0abbfa1890e95845c648f63722b7a1ae28caf
Amit Kumar <amitasviper@gmail.com>
2018-05-21 13:08:27 -0700

    Fix the bug which was skipping few stages in LL Calculation

--- GUI/utils/lesion_load_calculation_operation.py
+++ GUI/utils/lesion_load_calculation_operation.py
@@ -6,45 +6,43 @@ class LesionLoadCalculationOperation(BaseOperation):
 	def runLesionLoadCalculation(self, anatomical_id, lesion_mask_id):
 		#standard_brain = self.controller.sv_user_brain_template.get()
 		#template_brain = os.path.join(self.getProjectDirectory(), 'ROIs', 'MNI_FS_T1.nii.nii.gz')
+		if self.stage in (6, 7):
+			if self.controller.b_own_rois.get() == True:
+				space = 'custom'
+				template_brain = self.controller.sv_user_brain_template.get()
+				if self.stage == 6:
+					self.runReg(template_brain, space, anatomical_id, lesion_mask_id)
+				if self.stage == 7:
+					roi_list = self.controller.user_roi_paths
+					self._runLesionLoadCalculationHelper(space, roi_list, anatomical_id, lesion_mask_id)
+					self.incrementStage()
+			else:
+				self.incrementStage(2)
 
-		if self.controller.b_own_rois.get() == True:
-			space = 'custom'
-			template_brain = self.controller.sv_user_brain_template.get()
-			if self.stage == 6:
-				self.runReg(template_brain, space, anatomical_id, lesion_mask_id)
-			if self.stage == 7:
-				roi_list = self.controller.user_roi_paths
-				self._runLesionLoadCalculationHelper(space, roi_list, anatomical_id, lesion_mask_id)
-				self.incrementStage()
-		else:
-			self.incrementStage()
-			self.incrementStage()
+		if self.stage in (8, 9):
+			if self.controller.b_default_rois.get() == True:
+				space = 'MNI152'
+				template_brain = os.path.join(self.getProjectDirectory(), 'ROIs', 'MNI152_T1_2mm_brain.nii.gz')
+				if self.stage == 8:
+					self.runReg(template_brain, space, anatomical_id, lesion_mask_id)
+				if self.stage == 9:
+					roi_list = self.controller.default_roi_paths
+					self._runLesionLoadCalculationHelper(space, roi_list, anatomical_id, lesion_mask_id)
+					self.incrementStage()
+			else:
+				self.incrementStage(2)
 
-		if self.controller.b_default_rois.get() == True:
-			space = 'MNI152'
-			template_brain = os.path.join(self.getProjectDirectory(), 'ROIs', 'MNI152_T1_2mm_brain.nii.gz')
-
-			if self.stage == 8:
-				self.runReg(template_brain, space, anatomical_id, lesion_mask_id)
-			if self.stage == 9:
-				roi_list = self.controller.default_roi_paths
-				self._runLesionLoadCalculationHelper(space, roi_list, anatomical_id, lesion_mask_id)
-				self.incrementStage()
-		else:
-			self.incrementStage()
-			self.incrementStage()
-
-		if self.controller.b_freesurfer_rois.get() == True:
-			space = 'FS'
-			template_brain = ''
-			if self.stage == 10:
-				self.runReg(template_brain, space, anatomical_id, lesion_mask_id)
-			if self.stage == 11:
-				self.runLesionLoadCalculationFS(space)
-				self.incrementStage()
-		else:
-			self.incrementStage()
-			self.incrementStage()
+		if self.stage in (10, 11):
+			if self.controller.b_freesurfer_rois.get() == True:
+				space = 'FS'
+				template_brain = ''
+				if self.stage == 10:
+					self.runReg(template_brain, space, anatomical_id, lesion_mask_id)
+				if self.stage == 11:
+					self.runLesionLoadCalculationFS(space)
+					self.incrementStage()
+			else:
+				self.incrementStage(2)
 
 		if self.stage == 12:
 			image_files_base = os.path.join(self.getBaseDirectory(), 'QC_Registrations', space)
@@ -94,8 +92,8 @@ class LesionLoadCalculationOperation(BaseOperation):
 				cmd = 'convert_xfm -omat %s %s;'%(xfm_inverse_file, reg_file)
 				self.com.runRawCommand(cmd)
 
-		html_file_path = image_files_base = os.path.join(self.getBaseDirectory(), 'QC_Registrations', space)
-		generateQCPage('Registration', image_files_base)
+		image_files_base = os.path.join(self.getBaseDirectory(), 'QC_Registrations', space)
+		html_file_path = generateQCPage('Registration', image_files_base)
 		self.printQCPageUrl('runReg', html_file_path)
 
 

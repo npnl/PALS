85fe3f2f56dc9f43a737983fa117c479cd225a93
Lex <50920802+AlexandreHutton@users.noreply.github.com>
2022-01-21 12:39:03 -0800

    restructured package; added setup + cmdline PALS

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='PALS',
+    version='2.0.0',
+    packages=['PALS'],
+    url='https://github.com/npnl/PALS',
+    license='MIT',
+    author='AlexandreHutton',
+    author_email='ahutton@usc.edu',
+    description='Pipeline for Analyzing Lesions after Stroke',
+    entry_points={
+        'console_scripts': ['PALS=PALS.pals_workflow:main']
+    }
+)

d93139a5c2419d6d76f1499782fd94401cbf392a
Lex <50920802+AlexandreHutton@users.noreply.github.com>
2022-03-23 14:38:39 -0700

    removed vestigial SQL code; fixed command-line session parsing; updated setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,21 @@ from setuptools import setup
 
 setup(
     name='PALS',
-    version='2.0.2',
+    version='2.0.6',
     packages=['PALS'],
     url='https://github.com/npnl/PALS',
     license='MIT',
     author='AlexandreHutton',
     author_email='ahutton@usc.edu',
     description='Pipeline for Analyzing Lesions after Stroke',
+    install_requires=[
+        'bids',
+        'nibabel>=3.2.1',
+        'numpy>=1.2.1',
+        'pandas>=1.3.2',
+        'scipy>=1.7.1',
+        'nipype>=1.7.0'
+    ],
     entry_points={
         'console_scripts': ['PALS=PALS.pals_workflow:main']
     }
